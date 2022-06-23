7b8b6bd7c3b40ce36d313ecf4bf4e69c184418b5
CarlosSantosBarros <31740094+CarlosSantosBarros@users.noreply.github.com>
2022-06-23 17:31:22 +0100

    Chores
    Folder structure, editor settings

---
+++ Controllers/RootController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Short.er.Models;
+using System.Diagnostics;
+
+namespace Short.er.Controllers {
+  public class RootController : Controller {
+
+    private readonly ILogger<RootController> _logger;
+
+    public RootController(ILogger<RootController> logger) {
+      _logger = logger;
+    }
+    [Route("")]
+    public IActionResult Index() {
+      return View();
+    }
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error() {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+  }
+}
